beab3cdcadc96e326d2b097d85d85d8fdb65e719
Max S <max-schweigerdt@web.de>
2018-11-05 22:54:34 +0100

    Setup Material SCSS & create base layout

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
 import { NgModule } from '@angular/core';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+  MatToolbarModule, MatButtonModule, MatSidenavModule, MatInputModule, MatIconModule,
+  MatListModule, MatGridListModule, MatMenuModule, MatFormFieldModule, MatTabsModule, MatCardModule
+} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { HeaderComponent } from './layouts/header/header.component';
+import { SideNavComponent } from './layouts/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    BrowserModule,
+    LayoutModule,
+    MatCardModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatInputModule,
+    MatListModule, MatGridListModule, MatMenuModule, MatFormFieldModule, MatTabsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
